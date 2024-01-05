44e2aca992a9de8ac1fd170d661245bb1d60e896
Davit Danielyan <davit-danielyan@hotmail.com>
2024-01-05 14:57:39 +0100

    changes in fetch

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,9 +2,21 @@ import ChuckNorrisJoke from '../components/ChuckNorrisJoke';
 
 const IndexPage: React.FC = () => {
   const getJoke = async (): Promise<string> => {
-    const response = await fetch('https://weather-app-davitdanielyan-e3uzzxqbva-uc.a.run.app');
-    const data = await response.json();
-    return data;
+    try {
+      const response = await fetch(
+        'https://weather-app-davitdanielyan-e3uzzxqbva-uc.a.run.app/',
+      );
+
+      if (!response.ok) {
+        throw new Error(`Failed to fetch data. Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      return 'Failed to fetch Chuck Norris Joke'; // You can handle the error gracefully
+    }
   };
 
   return <ChuckNorrisJoke getJoke={getJoke} />;
