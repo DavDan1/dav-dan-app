bfc73511937446925b62176b05f5bce5ad62dc4f
Davit Danielyan <davit-danielyan@hotmail.com>
2024-01-05 13:33:05 +0100

    refactor program.cs

--- webApiDemo/Program.cs
+++ webApiDemo/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddHttpClient<ChuckNorrisService>();
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,12 +16,14 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 var url = $"http://0.0.0.0:{port}";
 
